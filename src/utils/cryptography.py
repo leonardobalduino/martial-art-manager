f788b81a88b050b9d663465ad341061ece8f0b4f
leonardo.balduino <leonardo.balduino@luizalabs.com>
2021-05-01 10:58:59 -0300

    feat: add crypthography

---
+++ src/utils/cryptography.py
@@ -0,0 +1,22 @@
+from cryptography.fernet import Fernet
+
+from src.utils.settings import get_cryptography_key
+
+KEY = get_cryptography_key()
+
+
+def _get_instance_cryptography():
+    f = Fernet(KEY)
+    return f
+
+
+def encrypt(text: str):
+    f = _get_instance_cryptography()
+    enc_text = f.encrypt(text.encode()).decode()
+    return str(enc_text)
+
+
+def decrypt(text: str):
+    f = _get_instance_cryptography()
+    dec_text = f.decrypt(text.encode()).decode()
+    return str(dec_text)
